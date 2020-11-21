ab6c4ea9e999a8eddb14e99fb7ec18a47439078f
Jared Stigter <jaredstigter@gmail.com>
2020-11-20 20:03:07 -0800

    Restructured files to get rid of CPUBets class

--- MultiThreading/src/multithread/Bet.java
+++ MultiThreading/src/multithread/Bet.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class Bet extends Thread{
 	private int id, ownerId, steps; 
@@ -56,6 +57,70 @@ public class Bet extends Thread{
 		{ 
 			e.printStackTrace();
 		}
+		
+		//sleep for 10-15min
+		Thread.sleep((long)(10+Math.random()*5)*60*1000);
+		
+		//settle the bet
+		settleBets();
+		
+		//etc 
+		distributePoints();
+	}
+	
+	public void settleBets(ArrayList<Bet> bets) { 
+		String SQL = "UPDATE public.bet "
+                + "SET active = ? "
+                + "WHERE bet_owner_user_id = 0";
+		//public.bet(bet_id, bet_owner_user_id, title, description, steps_wagered, date_created, active, achieved_goal)"
+		try (Connection conn = DriverManager.getConnection(JDBCMain.url, JDBCMain.user, JDBCMain.pwd); 
+                PreparedStatement pstmt = conn.prepareStatement(SQL)) {
+			int count = 0;
+			for(Bet b: bets) 
+			{ 
+				 pstmt.setBoolean(1, false);
+				 doneBets.add(b);
+				 pstmt.addBatch(); //check if you need this for Update
+				 count++;
+			}
+
+			if(count == bets.size())
+			{ 
+				pstmt.executeUpdate();
+			}
+			
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+		
+	}
+	public void distributePoints() 
+	{ 
+		String SQL = "UPDATE user "
+                + "SET points = ? "
+				+ "FROM public.user user, public.user_bets userbets, public.bet bet "
+                + "WHERE bet.bet_id = userbets.bet_id"
+				+ "AND userbets.user_id = 0";
+		//public.bet(bet_id, bet_owner_user_id, title, description, steps_wagered, date_created, active, achieved_goal)"
+		try (Connection conn = DriverManager.getConnection(JDBCMain.url, JDBCMain.user, JDBCMain.pwd); 
+                PreparedStatement pstmt = conn.prepareStatement(SQL)) {
+			int count = 0;
+			for(Bet b: doneBets) 
+			{ 
+				pstmt.setInt(1, b.getSteps()*2);
+				pstmt.addBatch();
+				count++;
+				
+			}
+			if(count == doneBets.size()) 
+			{ 
+				pstmt.executeBatch();
+			}
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
 		
 	}
 
--- MultiThreading/src/multithread/JDBCMain.java
+++ MultiThreading/src/multithread/JDBCMain.java
@@ -2,23 +2,16 @@ package multithread;
 
 public class JDBCMain {
 	
-	public static String url = "jdbc:postgresql://localhost:5555/fitbet?useUnicode=true&useJDBCCompliantTimezoneShift="
-			+ "true&useLegacyDatetimeCode=false&serverTimezone=America/Los_Angeles";
+	public static String url = "jdbc:postgresql://localhost:5555/fitbet?stringtype=unspecified";
 	public static String user = "postgres";
 	public static String pwd = "postgres";
 	public static void main(String[] args) {
+		System.out.println("Hello there jared");
 		// TODO Auto-generated method stub
 
 		Checker check = new Checker(); 
-		CPUBets cpuBet = new CPUBets();
 	
-		check.start(); 
-		cpuBet.start();
-		
-		
-		
-		
-		
+		check.run(); 
 	}
 
 }
